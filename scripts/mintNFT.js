7c4b16a45f7a90ceedcd229812a4612373d3e0bb
rruuffuuss <rufusponniah@icloud.com>
2024-03-09 20:39:37 +0000

    Use env varibale wallet address

--- scripts/mintNFT.js
+++ scripts/mintNFT.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
-    const myToken = await ethers.getContractAt("MyToken", "YOUR_CONTRACT_ADDRESS_HERE");
+    const myToken = await ethers.getContractAt(process.env.YOUR_WALLET_ADDRESS);
 
     const uri = "https://ipfs.io/ipfs/QmPjyATk9s5BsZMUD82eWYCEWGmuCXAQjarBirGMrH4eE5?filename=luxap_exam.json";
     await myToken.mintRealEstate(deployer.address, 1, uri, "0x");
