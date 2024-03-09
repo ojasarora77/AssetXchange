5041c5462fc1211b0130275a1fe849279feee9b0
rruuffuuss <rufusponniah@icloud.com>
2024-03-09 21:50:34 +0000

    test mintRealEstate NFT

--- scripts/mintNFT.js
+++ scripts/mintNFT.js
@@ -1,9 +1,18 @@
-const { ethers } = require("hardhat");
+const wallet = '0x426F990461C72f028c32F70ebf30208aCa24c067';
+const { ethers, artifacts } = require("hardhat");
 
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    const myToken = await ethers.getContractAt(process.env.YOUR_WALLET_ADDRESS);
+    // Dynamically fetch the ABI at runtime
+    const MyToken = await ethers.getContractFactory("MyToken"); // Replace "MyToken" with the name of your contract
+    const myToken = await MyToken.attach(wallet);
 
+    // You can access the ABI via the deployed contract object
+    const contractABI = MyToken.interface.format.abi;
+
+    console.log(contractABI);
+    console.log("hello");
+    
+    const [deployer] = await ethers.getSigners();
     const uri = "https://ipfs.io/ipfs/QmPjyATk9s5BsZMUD82eWYCEWGmuCXAQjarBirGMrH4eE5?filename=luxap_exam.json";
     await myToken.mintRealEstate(deployer.address, 1, uri, "0x");
 }
