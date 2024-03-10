bef480a9d7641bcbf710b4a7222d29a620058ce1
Dawe000 <100613136+Dawe000@users.noreply.github.com>
2024-03-10 16:49:19 +0000

    added frontend files

---
+++ frontend/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  );
+}
