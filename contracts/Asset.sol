03ea4963d591a7417135aceee710b3a2c684c776
rruuffuuss <rufusponniah@icloud.com>
2024-03-10 06:54:10 +0000

    created asset management smart contract

---
+++ contracts/Asset.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AssetType.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract Asset is ERC721 {
+    mapping (uint256 => string[]) assetData;
+    mapping (uint256 => string) assetURI;
+
+
+    uint256 private tokenIdNo;
+
+    uint256 private assetTypeToken;
+    AssetType private assetType;
+
+    constructor(uint256 token, address _assetTypeAddress) ERC721("AssetXchange Asset", "AXCA") {
+        tokenIdNo = 0;
+        assetTypeToken = token;
+        assetType = AssetType(_assetTypeAddress);
+    }
+
+    function createAsset(address account, string[] memory assetProperties, string memory URI) external returns (uint256) {
+        require(assetType.checkAssetTypeOwner(msg.sender, assetTypeToken), "Only the owner of the asset type can create assets");
+        
+        tokenIdNo += 1;
+        _mint(account,tokenIdNo);
+        assetData[tokenIdNo] = assetProperties;
+        assetURI[tokenIdNo] = URI;
+        return tokenIdNo;
+    }
+
+    function updateAsset(uint256 token, string[] memory assetProperties, string memory URI) external {
+        require(msg.sender == ownerOf(token), "Only the owner of the asset can update the asset");
+        assetData[token] = assetProperties;
+        assetURI[token] = URI;
+    }
+
+    function burnAsset(uint256 token) external {
+        require(msg.sender == ownerOf(token), "Only the owner of the asset can burn the asset");
+        _burn(token);
+    }
+
+    function getAssetData(uint256 token) public view returns(string[] memory){
+        return assetData[token];
+    }
+
+    function getAssetTypeData(uint256 token) public view returns(string[] memory){
+        return assetType.getAssetTypeData(token);
+    }
+
+    function tokenURI(uint256 token) public override view returns(string memory){
+        return assetURI[token];
+    }
+
+    function getAssetTypeURI(uint256 token) public view returns(string memory){
+        return assetType.tokenURI(token);
+    }
+
+
+}
---
+++ contracts/AssetType.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./Asset.sol";
+
+contract AssetType is ERC721 {
+    event AssetCreated(address asset);
+
+    mapping (uint256 => string[]) assetTypeData;
+    mapping (uint256 => string) assetTypeURI;
+    uint256 private tokenIdNo;
+
+    constructor() ERC721("AssetXchange AssetType", "AXCT") {
+        tokenIdNo = 0;
+    }
+
+    // Create a new type of asset
+    function createAssetType(address account, string[] memory assetPropertyNames, string memory URI) external returns(uint256){
+        tokenIdNo += 1;
+        _mint(account, tokenIdNo);
+
+        assetTypeData[tokenIdNo] = assetPropertyNames;
+        assetTypeURI[tokenIdNo] = URI;
+
+        Asset a = new Asset(tokenIdNo, address(this));
+        emit AssetCreated(address(a));
+        return tokenIdNo;
+    }
+
+    function checkAssetTypeOwner(address sender, uint256 assetType) public view returns(bool){
+        return sender==ownerOf(assetType);
+    }
+
+    // Getter function for assetTypeData
+    function getAssetTypeData(uint256 tokenId) public view returns (string[] memory) {
+        return assetTypeData[tokenId];
+    }
+
+    // Getter function for assetTypeURI
+    function getAssetTypeURI(uint256 tokenId) public view returns (string memory) {
+        return assetTypeURI[tokenId];
+    }
+
+    function tokenURI(uint256 token) public override  view returns(string memory){
+        return assetTypeURI[token];
+    }
+}
